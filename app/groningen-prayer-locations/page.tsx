868c2ab4ba2dc702e06bd9bff90f73b0c5731230
nomomon <funisinfinity26@gmail.com>
2024-06-30 00:58:02 +0500

    add metadata

--- app/groningen-prayer-locations/page.tsx
+++ app/groningen-prayer-locations/page.tsx
@@ -1,3 +1,10 @@
 import PrayerLocations from "@/components/templates/prayer-locations";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Groningen Prayer Locations",
+  description:
+    "Find places to pray in Groningen, Netherlands. Mosques, prayer rooms, and more.",
+};
 
 export default PrayerLocations;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import About from "@/components/templates/about";
 import Projects from "@/components/templates/projects";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "BB Jamaat",
+  description: "",
+};
 
 export default function Home() {
   return (
